837cfff7c6b245ff483308935366474df25df863
rajaram <rajaramsharma422@gmail.com>
2022-12-11 00:26:32 +0530

    update build

--- build.sh
+++ build.sh
@@ -8,42 +8,21 @@ log(){
 }
 build(){
     log $1 "compiling"
-    local PASSES=3
-    for i in $(seq $PASSES); do
-        if [[ $i -eq $PASSES ]]; then
-			latexmk -interaction=nonstopmode -pdf "$1.tex" >/dev/null 2>&1
-        else
-			latexmk -interaction=nonstopmode -pdf- "$1.tex" >/dev/null 2>&1
-        fi
-        if [[ $? -ne 0 ]]; then
-			log $1 "failed"
-            return 1
-        fi
-        if [[ $i -eq 1 ]]; then
-			bibtex $1 >/dev/null 2>&1
-        fi
-    done
+    pdflatex -interaction=nonstopmode "$1.tex" >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        log $1 "failed"
+        return 1
+    fi
     log $1 "compiled successfully"
-    mv -f "$1.pdf" .. >/dev/null 2>&1
-	mv -f "$1.tex" .. >/dev/null 2>&1
     return 0
 }
 #bash start
-command -v latexmk > /dev/null
+command -v pdflatex > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "Latexmk is missing, texlive-full may not be installed"
     return 1
 fi;
-command -v bibtex > /dev/null
-if [[ $? -ne 0 ]]; then
-    echo "Bibtex is missing, texlive-full may not be installed"
-    return 1
-fi;
 echo "$(tput setaf 11)Build started$(tput sgr 0)"
-mkdir latex >/dev/null 2>&1
-mv -f $1 latex >/dev/null 2>&1
-cd latex
 build Rajaram_resume &
 wait
-cd ..
 echo "Build completed in $SECONDS seconds"
